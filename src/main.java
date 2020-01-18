80abcd9a04e1bfbc6441bdd7892f5a1c60ee4837
GStrongman <georgia.strongman@gmail.com>
2020-01-18 23:02:03 +1300

    The end result text will now be saved to the user's clipboard, to be pasted away to their heart's content.

--- src/main.java
+++ src/main.java
@@ -1,8 +1,14 @@
+import java.awt.*;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.StringSelection;
+import java.awt.datatransfer.Transferable;
+import java.util.Arrays;
 import java.util.Scanner;
 
 /**
  * Very simple program to read console input and capitalise each
- * second character, then print the result out.
+ * second character, then print the result out and save the result
+ * to the user's clipboard.
  */
 
 public class main {
@@ -16,6 +22,15 @@ public class main {
                a[i] = Character.toUpperCase(a[i]);
             }
         }
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(a);
+        text = sb.toString();
+
+        Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+        Transferable transferable = new StringSelection(text);
+        clipboard.setContents(transferable, null);
+
         System.out.print(a);
     }
 }
